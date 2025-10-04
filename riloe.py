113eba83df668e2066f20a329037747a679643f3
Iago Laz <iago.laz@gmail.com>
2025-10-04 22:51:42 +0200

    feat: primeira versión do script

---
+++ riloe.py
@@ -0,0 +1,9 @@
+def main():
+    parser = argparse.ArgumentParser(
+        description="Xunta varios ficheiros markdown de Obsidian nun EPUB.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+
+if __name__ == "__main__":
+    main()
